d40586c2485c1e066592663f451b496a237ac9f6
DadoLinux <ziocleto@gmail.com>
2020-03-28 03:39:49 +0000

    added confirmAlertWithWriteCheck

--- alerts/alerts.js
+++ alerts/alerts.js
@@ -1,17 +1,80 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 import Button from 'react-bootstrap/Button'
 import Modal from "react-bootstrap/Modal";
 import Alert from "react-bootstrap/Alert";
 import {getRandomMovieQuote, getRandomMovieQuotePositive} from "../warningsmoviequotes/warningMovieQuotes";
 import {useGlobal} from "reactn";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 
 export const ConfirmAlert = 'confirmAlert';
 export const NotificationAlert = 'notificationAlert';
+export const ConfirmAlertWithWriteCheck = 'ConfirmAlertWithWriteCheck';
 
 export const EHAlert = () => {
 
   const [confirmAlert, setConfirmAlert] = useGlobal(ConfirmAlert);
+  const [confirmAlertWithWriteCheck, setConfirmAlertWithWriteCheck] = useGlobal(ConfirmAlertWithWriteCheck);
   const [notificationAlert, setNotificationAlert] = useGlobal(NotificationAlert);
+  const [modalConfirmWithWriteDeleteButtonEnabled, setModalConfirmWithWriteDeleteButtonEnabled] = useState(false);
+
+  if (confirmAlertWithWriteCheck) {
+    return (
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={true}
+        onClose={() => {
+          setConfirmAlertWithWriteCheck(null).then();
+          setModalConfirmWithWriteDeleteButtonEnabled(false);
+        }}
+      >
+        {confirmAlertWithWriteCheck.title && <Modal.Header closeButton onClick={ () => {
+          setModalConfirmWithWriteDeleteButtonEnabled(false);
+          setConfirmAlertWithWriteCheck(null);
+        }}>
+          <Modal.Title>{confirmAlertWithWriteCheck.title}</Modal.Title>
+        </Modal.Header>}
+        <Modal.Body>
+          <div>
+          To delete <b>{confirmAlertWithWriteCheck.text}</b> type it below to <b>confirm</b> it.
+          </div>
+          <InputGroup className="my-3">
+            <FormControl
+              onChange={ (e) => {
+                const isSame = e.target.value === confirmAlertWithWriteCheck.text;
+                console.log(isSame);
+                console.log(e.target.value);
+                if ( modalConfirmWithWriteDeleteButtonEnabled !== isSame ) {
+                  setModalConfirmWithWriteDeleteButtonEnabled(isSame);
+                }
+              } }
+            />
+          </InputGroup>
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={ () => {
+            setModalConfirmWithWriteDeleteButtonEnabled(false);
+            setConfirmAlertWithWriteCheck(null);
+          }}>
+            {confirmAlertWithWriteCheck.noText}
+          </Button>
+          <Button
+            disabled={!modalConfirmWithWriteDeleteButtonEnabled}
+            variant={confirmAlertWithWriteCheck.yesType}
+            onClick={ () => {
+              confirmAlertWithWriteCheck.yesCallback();
+              setConfirmAlertWithWriteCheck(null).then();
+              setModalConfirmWithWriteDeleteButtonEnabled(false);
+            }}>
+            {confirmAlertWithWriteCheck.yesText}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
 
   if (confirmAlert) {
     return (
@@ -28,14 +91,15 @@ export const EHAlert = () => {
         </Modal.Header>}
         <Modal.Body>{confirmAlert.text}</Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={setConfirmAlert(null)}>
+          <Button variant="primary" onClick={ () => setConfirmAlert(null)}>
             {confirmAlert.noText}
           </Button>
           <Button variant={confirmAlert.yesType} onClick={confirmAlert.yesCallback}>
             {confirmAlert.yesText}
           </Button>
         </Modal.Footer>
-      </Modal>)
+      </Modal>
+    )
   }
 
   if (notificationAlert) {
@@ -70,13 +134,13 @@ export const EHAlert = () => {
 };
 
 export const useAlert = () => {
-  const [,store] = useGlobal(NotificationAlert);
+  const [, store] = useGlobal(NotificationAlert);
   return store;
 };
 
 export const useAlertWarning = (title) => {
 
-  const [,store] = useGlobal(NotificationAlert);
+  const [, store] = useGlobal(NotificationAlert);
 
   store({
     title: title,
