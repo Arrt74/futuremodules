530f2a657a08903c0bba87b050a863b0541603bb
DadoLinux <ziocleto@gmail.com>
2020-04-13 00:17:19 +0100

    Update fetchApiCalls.js

--- fetch/fetchApiCalls.js
+++ fetch/fetchApiCalls.js
@@ -3,13 +3,18 @@ import {createAntiForgeryTokenHeaders} from "../auth/authAccessors";
 
 const getCSV = async (body) => {
   return await axios.post(`/gapi/fetch/csvkeys`, body);
-}
+};
 
 const getCSVGraphKeys = async (body) => {
   return await axios.post(`/gapi/fetch/csvgraphkeys`, body, createAntiForgeryTokenHeaders());
-}
+};
+
+const putScript = async (body) => {
+  return await axios.put(`/gapi/fetch/script`, body, createAntiForgeryTokenHeaders());
+};
 
 export {
   getCSV,
-  getCSVGraphKeys
+  getCSVGraphKeys,
+  putScript
 }
