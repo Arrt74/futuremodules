52985f8d7a64334c4060274ebd69b44aaa9e6a6d
DadoLinux <ziocleto@gmail.com>
2020-04-18 04:34:01 +0100

    removed unused function

--- graphqlclient/query.js
+++ graphqlclient/query.js
@@ -1,17 +1,3 @@
-import {useQuery} from "@apollo/react-hooks";
-import {useGlobal} from "reactn";
-
-export const useQueryData = (query) => {
-  const [loadingQuery, setLoading] = useGlobal('loadingQuery');
-
-  const {data, loadingStatus} = useQuery(query);
-  if (loadingStatus !== loadingQuery) {
-    setLoading(loadingStatus);
-  }
-
-  return data;
-};
-
 export const checkQueryHasLoadedWithData = queryResult => {
   return queryResult && queryResult.data && queryResult.loading === false;
 };
