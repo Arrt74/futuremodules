21e37bfe1cf32dcabf2a1822b2848d0682d7136c
DadoLinux <ziocleto@gmail.com>
2020-04-19 11:29:41 +0100

    Update NavbarUserAvatar.js

--- navbar/components/NavbarUserAvatar.js
+++ navbar/components/NavbarUserAvatar.js
@@ -1,7 +1,7 @@
 import {Link} from "react-router-dom";
 import {getUserName, logoffFromProject, useGetAuth} from "../../auth/authAccessors";
 import React from "react";
-import {Logo2TextSpan} from "../../reactComponentStyles/reactCommon.styled";
+import {Flex, Logo1TextSpanBold, Logo2TextSpanBold, Mx025} from "../../reactComponentStyles/reactCommon.styled";
 
 export const NavbarUserAvatar = () => {
 
@@ -12,7 +12,20 @@ export const NavbarUserAvatar = () => {
     <Link to="/dashboarduser" onClick={() => {
       logoffFromProject(auth);
     }}>
-      {userName && <span><span><i className="fas fa-user"/></span><Logo2TextSpan>{" "}{userName}</Logo2TextSpan></span>}
+      {userName && <Flex alignItems={"center"}>
+        <div>
+          <Logo1TextSpanBold
+            fontSize={"30px"}>
+            <i className="fas fa-user-circle"/>
+          </Logo1TextSpanBold>
+        </div>
+        <Mx025/>
+        <div>
+          <Logo2TextSpanBold>
+            {userName}
+          </Logo2TextSpanBold>
+        </div>
+      </Flex>}
     </Link>
   )
 };
