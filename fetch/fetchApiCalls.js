a4f3c655b381b38748c75b207b613f99de380864
DadoLinux <ziocleto@gmail.com>
2020-04-22 23:42:31 +0100

    fazing out fetchApi from future modules as it should have never been here :/

--- fetch/fetchApiCalls.js
+++ fetch/fetchApiCalls.js
@@ -1,35 +1,10 @@
 import axios from "axios";
 import {createAntiForgeryTokenHeaders} from "../auth/authAccessors";
 
-const getScripts = async (trendId) => {
-  return await axios.get(`/gapi/fetch/scripts/${trendId}`, createAntiForgeryTokenHeaders());
-};
-
-const getSimilarScripts = async (trendId) => {
-  return await axios.get(`/gapi/fetch/similar/${trendId}`, createAntiForgeryTokenHeaders());
-};
-
 const addNewScript = async (body) => {
   return await axios.post(`/gapi/fetch/script`, body, createAntiForgeryTokenHeaders());
 };
 
-const putScript = async (body) => {
-  return await axios.put(`/gapi/fetch/script`, body, createAntiForgeryTokenHeaders());
-};
-
-const patchScript = async (trendId, name) => {
-  return await axios.patch(`/gapi/fetch/script`, {trendId, name}, createAntiForgeryTokenHeaders());
-};
-
-const deleteScript = async (trendId, name) => {
-  return await axios.delete(`/gapi/fetch/script`, {...createAntiForgeryTokenHeaders(), data: {trendId, name}});
-};
-
 export {
-  getScripts,
-  getSimilarScripts,
   addNewScript,
-  putScript,
-  patchScript,
-  deleteScript
 }
