e1fd4a767aa258447ae91986b53e0a72d88a3dfc
DadoLinux <ziocleto@gmail.com>
2020-04-25 10:47:45 +0100

    increaded navbar spacing limit, just in case

--- navbar/components/navbar-styled.js
+++ navbar/components/navbar-styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const navbarJustifyEndsWidth = "200px";
+export const navbarJustifyEndsWidth = "300px";
 
 export const NavbarLeftHandSizeComponent = styled.div`{
   width: ${navbarJustifyEndsWidth};
