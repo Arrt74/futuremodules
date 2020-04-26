b249eaf20befae9433012ee3a849bc13a3e6636a
DadoLinux <ziocleto@gmail.com>
2020-04-26 23:07:03 +0100

    Update VideoPhoneChat.js

--- webrtc/components/VideoPhoneChat.js
+++ webrtc/components/VideoPhoneChat.js
@@ -1,20 +1,39 @@
 import React, {useEffect, withGlobal} from "reactn";
 import {getAuthUserName} from "../../auth/authAccessors";
-import {Button, ButtonGroup, Container, FormControl, InputGroup, Row} from "react-bootstrap";
+import {Button, ButtonGroup, FormControl, InputGroup, Row} from "react-bootstrap";
 import {connect, phoneCall, sendChatMessage} from "../client";
 import {useState} from "react";
-import {
-  InfoTextSpanBold,
-  LightColorTextSpan,
-  Video,
-  VideoPhoneChatContainer
-} from "../../reactComponentStyles/reactCommon.styled";
+import {FlexVertical, InfoTextSpanBold, LightColorTextSpan, Video} from "../../reactComponentStyles/reactCommon.styled";
+
+const Chat = ({currentChat}) => {
+  return (
+    <Row>
+      <ul>
+        {currentChat.map(elem => (<li><InfoTextSpanBold>{elem.username}</InfoTextSpanBold>: <LightColorTextSpan
+          fontSize={"0.75rem"}>{elem.text}</LightColorTextSpan></li>))}
+      </ul>
+      <InputGroup className="mb-3" id="text" type="text" name="text" maxLength="256" placeholder="..."
+                  autoComplete="off" onKeyUp={(evt) => {
+        if ((evt.keyCode === 13 || evt.keyCode === 14)) {
+          sendChatMessage(evt.target.value);
+          evt.target.value = "";
+        }
+      }}>
+        <InputGroup.Prepend>
+          <InputGroup.Text id="basic-addon1"><i className="fas fa-pencil-alt"/></InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl/>
+      </InputGroup>
+    </Row>
+  )
+}
 
 const VideoPhoneChat = (props) => {
 
   const auth = props.auth;
 
-  console.log("Auth:" + JSON.stringify(props));
+  const [showChat, setShowChat] = useState(null);
+  const [showVideoFeed, setShowVideoFeed] = useState(null);
   const [currentChat, setCurrentChat] = useState([]);
   const [wsconnection, setWSConnection] = useState(null);
   // const inChat = false;
@@ -39,40 +58,21 @@ const VideoPhoneChat = (props) => {
   // }
 
   return (
-    <VideoPhoneChatContainer>
-      <Container>
+    <FlexVertical>
+      {showVideoFeed && <>
         <Row className={"my-2"}>
           <Video id="received_video" width={"100%"} autoPlay/>
         </Row>
         <Row className={"my-2"}>
           <Video id="local_video" width={"100%"} autoPlay muted/>
-        </Row>
-        <Row>
-          <ul>
-            {currentChat.map(elem => (<li> <InfoTextSpanBold>{elem.username}</InfoTextSpanBold>: <LightColorTextSpan fontSize={"0.75rem"}>{elem.text}</LightColorTextSpan> </li>))}
-          </ul>
-          <InputGroup className="mb-3" id="text" type="text" name="text" maxLength="256" placeholder="..."
-                      autoComplete="off" onKeyUp={(evt) => {
-            if ((evt.keyCode === 13 || evt.keyCode === 14)) {
-              sendChatMessage(evt.target.value);
-              evt.target.value = "";
-            }
-          }}>
-            <InputGroup.Prepend>
-              <InputGroup.Text id="basic-addon1"><i className="fas fa-pencil-alt"/></InputGroup.Text>
-            </InputGroup.Prepend>
-            <FormControl/>
-          </InputGroup>
-        </Row>
-        <Row>
-          <ButtonGroup aria-label="">
-            <Button variant="primary"><i className={"fas fa-comments"}/>{" "}chat</Button>
-            <Button variant="info"><i className={"fas fa-phone-alt"}/>{" "}phone</Button>
-            <Button variant="success" onClick={() => makePhoneCall()}><i className={"fas fa-video"}/>{" "}video</Button>
-          </ButtonGroup>
-        </Row>
-      </Container>
-    </VideoPhoneChatContainer>
+        </Row></>}
+      {showChat && <Chat currentChat={currentChat}/>}
+      <ButtonGroup className={"w-100"}>
+        <Button variant="primary"><i className={"fas fa-comments"}/><br/>chat</Button>
+        <Button variant="info"><i className={"fas fa-phone-alt"}/><br/>phone</Button>
+        <Button variant="success" onClick={() => makePhoneCall()}><i className={"fas fa-video"}/><br/>video</Button>
+      </ButtonGroup>
+    </FlexVertical>
   )
 };
 
